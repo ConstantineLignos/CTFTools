dc64cbb303fca4046037517c1a69554664224215
Constantine Lignos <constantine.lignos@gmail.com>
2014-03-26 15:13:00 -0400

    Add acroread installation.

--- install_ctf_prereqs.sh
+++ install_ctf_prereqs.sh
@@ -3,3 +3,8 @@ yum install -y $(cat ctf_prereqs.txt)
 wget ftp://fr2.rpmfind.net/linux/sourceforge/x/xp/xprint/xprint/release%20009.001/xprint-2004-07-07-release_009_001-0.9.001.i386.rpm -O xprint_latest.rpm
 # Install it
 rpm -i xprint_latest.rpm
+# Get the latest acroread. This is not because acroread is actually
+# necessary, but because if you do not install a newer version the CTF
+# installer will install an out-of-date and insecure one.
+wget http://ardownload.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i486linux_enu.rpm
+rpm -i AdbeRdr9.5.5-1_i486linux_enu.rpm
